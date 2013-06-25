683c3aa11f2e3fb6ab2d17ec675362862c03d3ea
Casper <Casper@192.168.1.100>
2013-06-25 12:38:44 +0200

    test

--- gen/com/projectgroepzondernaam/mobileapp/frontend/R.java
+++ gen/com/projectgroepzondernaam/mobileapp/frontend/R.java
@@ -54,14 +54,14 @@ public final class R {
         public static final int over_hover48=0x7f02001e;
     }
     public static final class id {
-        public static final int EditText01=0x7f080015;
+        public static final int EditText01=0x7f080014;
         public static final int EditText02=0x7f08001a;
         public static final int TextView01=0x7f08001c;
         public static final int action_settings=0x7f08001d;
         public static final int button1=0x7f080007;
-        public static final int button2=0x7f080016;
-        public static final int button3=0x7f080013;
-        public static final int button4=0x7f080014;
+        public static final int button2=0x7f080015;
+        public static final int button3=0x7f080016;
+        public static final int button4=0x7f080013;
         public static final int button5=0x7f080019;
         public static final int button6=0x7f08001b;
         public static final int editText1=0x7f080018;
